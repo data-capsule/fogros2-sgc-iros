f1b6fa863ad1b49d960a94837aa11af7bf49775f
Eric Chen <kych@berkeley.edu>
2022-11-02 19:43:16 -0700

    recv doesn't work likely because of checksum

--- bench/cpp/example.cpp
+++ bench/cpp/example.cpp
@@ -20,11 +20,71 @@ struct gdp {
 } gdp_header;
 
 
+// Server side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <pthread.h>
+#define MAXLINE 1024
+
+void* receiving_thread(void* input) {
+	int sockfd;
+	struct sockaddr_in servaddr, cliaddr;
+
+	printf("Receiving thread is created\n");
+	// Creating socket file descriptor
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+		
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr)); 
+		
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(PORT);
+		
+	// Bind the socket with the server address
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+		
+	unsigned int len, n;
+	len = sizeof(cliaddr);
+	while (true) {
+	  struct gdp buffer = {0};
+	  n = recvfrom(sockfd, (void *)&buffer, MAXLINE,
+		       MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+		       &len);
+	  printf("packet is received\n");
+	  printf("Client : %d\n", buffer.payload);
+	}
+		
+	return 0;
+}
+
+
 int main(int argc, char const* argv[])
 {
 	int sock = 0, valread, client_fd;
 	struct sockaddr_in serv_addr;
 	char* hello = "Hello from client";
+
+	pthread_t ptid;
+  
+	// Creating a new thread
+	pthread_create(&ptid, NULL, &receiving_thread, NULL);
+	
 	struct gdp h = {0};
 	h.action = (char) 5;
 	h.payload = (char) 65;
@@ -54,13 +114,13 @@ int main(int argc, char const* argv[])
 		printf("\nConnection Failed \n");
 		return -1;
 	}
-    while(true){
-    	send(sock, (void*) &h, sizeof(h), 0);
+
+	while (true)
+	  {
+	    send(sock, (void*) &h, sizeof(h), 0);
 	    printf("Hello message sent\n");
-        sleep(1);
-    }
-	valread = read(sock, buffer, 1024);
-	printf("%s\n", buffer);
+	    sleep(1);
+	  }
 
 	// closing the connected socket
 	close(client_fd);
